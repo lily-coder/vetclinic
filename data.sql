77db051fc409745abc93a90fc860922a1c64820e
lily-coder <lilyondie@gmail.com>
2022-01-12 23:18:46 +0300

    Query multiple tables

--- data.sql
+++ data.sql
@@ -56,4 +56,42 @@ INSERT INTO animals
 
 	(id,name,date_of_birth,escape_attempts,neutered,weight_kg)
 
-	VALUES (10, ' Blossom', '1998-10-13', 3, TRUE, 17); 
+	VALUES (10, ' Blossom', '1998-10-13', 3, TRUE, 17);
+
+INSERT INTO owners (full_name, age)
+	VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age)
+	VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age)
+	VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age)
+	VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age)
+	VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age)
+	VALUES ('Jodie Whittaker', 38);
+
+INSERT INTO species (name)
+	VALUES ('Pokemon');
+INSERT INTO species (name)
+	VALUES ('Digimon');
+SELECT * FROM animals;
+BEGIN;
+UPDATE animals SET species_id = 1
+	WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 2
+	WHERE species_id IS NULL;
+COMMIT;
+
+BEGIN;
+	UPDATE animals SET owners_id = 1
+		WHERE name LIKE 'Agumon';
+	UPDATE animals SET owners_id = 2
+		WHERE id IN (2, 3);
+	UPDATE animals SET owners_id = 3
+		WHERE id IN (4, 11);
+	UPDATE animals SET owners_id = 4
+		WHERE id IN (5, 7, 10);
+	UPDATE animals SET owners_id = 5
+		WHERE id IN (8, 9);
+COMMIT;
--- schema.sql
+++ schema.sql
@@ -7,7 +7,35 @@ CREATE TABLE animals (
     escape_attempts INT,
     neutered BOOLEAN,
     weight_kg DECIMAL,
+    PRIMARY KEY(id)
 );
 
 ALTER TABLE animals 
     ADD species VARCHAR;
+
+CREATE TABLE owners(
+	id SERIAL PRIMARY KEY,                    
+	full_name VARCHAR,   
+	age INT
+);
+
+CREATE TABLE species(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR
+);
+
+ALTER TABLE animals DROP species;
+ALTER TABLE animals
+	ADD COLUMN species_id INT,
+	ADD CONSTRAINT fk_species
+	FOREIGN KEY (species_id) 
+	REFERENCES species(id)
+;
+
+ALTER TABLE animals 
+	ADD COLUMN owners_id INT,
+	ADD CONSTRAINT fk_owners
+	FOREIGN KEY (owners_id)
+	REFERENCES owners(id)
+; 
+SELECT * FROM animals;
