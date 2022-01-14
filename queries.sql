c280c9da57153cf0778e9b2bc2c1fb892859fa0a
lily-coder <lilyondie@gmail.com>
2022-01-14 22:51:44 +0300

    Answer query questions

--- queries.sql
+++ queries.sql
@@ -101,51 +101,70 @@ SELECT owners.full_name,
 SELECT * FROM animals;
 
 SELECT animals.name,
-    owners.full_name
-    FROM animals
-    JOIN owners
-    ON animals.owners_id = owners.id
-    WHERE full_name = 'MelodyPond';
+  date_of_visit
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animals_id
+  LEFT JOIN vets ON vets.id = visits.vets_id
+  WHERE vets.name = 'William Tatcher'
+  ORDER BY visits.date_of_visit DESC
+  LIMIT 1
 
-SELECT animals.name AS animal_name,
-    species.name
-    AS species_type
-    FROM animals
-    JOIN species
-    ON animals.species_id = species.id
-    WHERE species.name='Pokemon';
+SELECT DISTINCT animals.name
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animals_id
+  LEFT JOIN vets ON vets.id = visits.vet_id
+  WHERE vets.name = 'Stephanie Mendez';
 
-SELECT full_name,
-    animals.name
-    FROM owners
-    LEFT JOIN animals
-    ON animals.owners_id=owners.id;
+SELECT vets.*,
+  species.name
+  FROM vets
+  LEFT JOIN specializations ON vets.id = specializations.vet_id
+  LEFT JOIN species ON specializations.species_id = species.id;
 
-SELECT count(animals)
-    FROM animals
-    JOIN species
-    ON animals.species_id=species.id;
+SELECT animals.name,
+  visits.date_of_visit
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animal_id
+  LEFT JOIN vets ON vets.id = visits.vets_id
+  WHERE vets.name = 'Stephanie Mendez'
+  AND date_of_visit > '2020-04-01'
+  AND date_of_visit < '2020-08-30'
 
-SELECT animals.name
-    FROM animals
-    JOIN owners
-    ON animals.owners_id=owners.id
-    WHERE owners.full_name='Jennifer Orwell';
+SELECT animals.name,
+  count(animals.name)
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animals_id
+  ORDER BY count(animals.name) DESC
 
-SELECT animals.name, 
-    animals.escape_attempts
-    FROM animals
-    INNER JOIN owners
-    ON owners.id = animals.owners_id
-    WHERE owners.full_name = 'Dean Winchester'
-    AND animals.escape_attempts = 0;
+SELECT animals.name,
+  visits.date_of_visit
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animals_id
+  LEFT JOIN vets ON vets.id = visits.vets_id
+  WHERE vets.name = 'Maisy Smith'
+  ORDER BY visits.date_of_visit ASC
+  LIMIT 1
 
-SELECT owners.full_name, 
-    COUNT(animals.name)
-    AS total_animals
-    FROM owners
-    JOIN animals
-    ON animals.owners_id=owners.id
-    GROUP BY owners.full_name
-    ORDER BY total_animals
-    DESC LIMIT 1;
+SELECT animals.*,
+  vets.*,
+  visits.date_of_visit
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animals_id
+  LEFT JOIN vets ON vets.id = visits.vets_id
+  ORDER BY visits.date_of_visit DESC
+  LIMIT 1;
+
+SELECT count(*)
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animals_id
+  LEFT JOIN vets ON vets.id = visits.vets_id
+  WHERE animals.species_id NOT IN (SELECT species_id FROM specializations WHERE vets_id = vets.id)
+
+SELECT species.name,
+  count(*)
+  FROM visits
+  LEFT JOIN animals ON animals.id = visits.animals_id
+  LEFT JOIN species ON animals.species_id = species.id
+  LEFT JOIN vets ON vets.id = visits.vets_id
+  WHERE vets.name = 'Maisy Smith'
+  GROUP BY species.name
